dcad768cae88e0b02f052a1e1e3a10667ccdb4a7
juanchovelezpro <velezolaya2012@hotmail.com>
2021-05-20 18:56:41 -0500

    Update script

--- setup.sh
+++ setup.sh
@@ -17,30 +17,36 @@ mkdir exported-models
 mkdir images
 mkdir models
 mkdir pre-trained-models
-cp /home/bragapo23/Pipeline-ObjectDetection/files/exporter_main_v2.py .
-cp /home/bragapo23/Pipeline-ObjectDetection/files/model_main_tf2.py .
+cp /home/juanchovelezpro/Desktop/Pipeline-ObjectDetection/files/exporter_main_v2.py .
+cp /home/juanchovelezpro/Desktop/Pipeline-ObjectDetection/files/model_main_tf2.py .
 cd annotations
-cp /home/bragapo23/Pipeline-ObjectDetection/files/label_map.pbtxt .
+cp /home/juanchovelezpro/Desktop/Pipeline-ObjectDetection/files/label_map.pbtxt .
 echo "Downloading train.record"
 gdown https://drive.google.com/uc?id=1Kl4dv4tg4ppFYfZebuo9WkHpSngkAXYa
 echo "Downloading test.record"
 gdown https://drive.google.com/uc?id=1Kn9Xbeurba9a5iBt0l8GsCrHwn5hFLE_
 cd ..
 cd pre-trained-models
+echo "Downloading pre-trained-model"
 wget http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.tar.gz
+tar -xf ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.tar.gz
+rm ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.tar.gz
 cd ..
-cd ..
+cd models
+mkdir my_ssd_resnet50_v1_fpn
+cp /home/juanchovelezpro/Desktop/Pipeline-ObjectDetection/files/pipeline.config .
+cd /home/juanchovelezpro/Desktop/TensorFlow
 
-# echo "Cloning TensorFlow Models into TensorFlow folder"
-# git clone https://github.com/tensorflow/models.git
-# cd models
-# echo "Checkout to older version for compatibility (20 sept 2020)"
-# git checkout -f c3d53565f3b5964e7ab46cb9a23689ceacc8823a
+echo "Cloning TensorFlow Models into TensorFlow folder"
+git clone https://github.com/tensorflow/models.git
+cd models
+echo "Checkout to older version for compatibility (20 sept 2020)"
+git checkout -f c3d53565f3b5964e7ab46cb9a23689ceacc8823a
 
-# echo "Installing some libraries and tools..."
-# apt-get install protobuf-compiler python-lxml python-pil
-# pip install Cython pandas tf-slim lvis
+echo "Installing some libraries and tools..."
+apt-get install protobuf-compiler python-lxml python-pil
+pip3 install Cython pandas tf-slim lvis
 
-# echo "Compiling protobuf libraries"
-# cd /TensorFlow/models/research/
-# protoc object_detection/protos/*.proto --python_out=.
+echo "Compiling protobuf libraries"
+cd /home/juanchovelezpro/Desktop/TensorFlow/models/research/
+protoc object_detection/protos/*.proto --python_out=.
