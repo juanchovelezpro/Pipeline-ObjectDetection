84d801754dabb1c3b396b33fbc16f66edac00446
juanchovelezpro <velezolaya2012@hotmail.com>
2021-05-21 02:01:41 -0500

    Fix build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-cd /home/bragapo23/TensorFlow/workspace/training_demo/pre-trained-models
+cd /home/bragapo23/TensorFlow/workspace/training_demo/pre-trained-model
 echo "Downloading pre-trained-model"
 wget http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.tar.gz
 tar -xf ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.tar.gz
