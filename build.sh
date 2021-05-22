a7d0cb21afc6c5f0323a08bec209102a0c1c6272
juanchovelezpro <velezolaya2012@hotmail.com>
2021-05-21 20:10:51 -0500

    Improving scripts

--- build.sh
+++ build.sh
@@ -1,14 +1,22 @@
-cd /home/bragapo23/TensorFlow/workspace/training_demo/pre-trained-model
+path=$(pwd)
+linkPreTrainedModel=http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.tar.gz
+tarPreTrainedModel=ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.tar.gz
+nameCustomModel=my_ssd_resnet50_v1_fpn
+
+echo "Building pipeline in folder -> $path/TensorFlow" 
+cd $path/TensorFlow/workspace/training_demo/pre-trained-model
 echo "Downloading pre-trained-model"
-wget http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.tar.gz
-tar -xf ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.tar.gz
-rm ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.tar.gz
+wget $preTrainedModel
+tar -xf $tarPreTrainedModel
+rm $tarPreTrainedModel
 cd ..
 cd models
-mkdir my_ssd_resnet50_v1_fpn
-cd my_ssd_resnet50_v1_fpn
-cp /home/bragapo23/Pipeline-ObjectDetection/files/pipeline.config .
-cd /home/bragapo23/TensorFlow
+echo "Creating folder for custom model"
+mkdir $nameCustomModel
+cd $nameCustomModel
+echo "Copying custom pipeline.config for training"
+cp $path/Pipeline-ObjectDetection/files/pipeline.config .
+cd $path/TensorFlow
 
 echo "Cloning TensorFlow Models into TensorFlow folder"
 git clone https://github.com/tensorflow/models.git
@@ -21,5 +29,5 @@ sudo apt-get install protobuf-compiler python-lxml python-pil
 pip3 install Cython pandas tf-slim lvis
 
 echo "Compiling protobuf libraries"
-cd /home/bragapo23/TensorFlow/models/research/
+cd $path/TensorFlow/models/research/
 protoc object_detection/protos/*.proto --python_out=.
--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
+path=$(pwd)
+trainRecord=https://drive.google.com/uc?id=1Kl4dv4tg4ppFYfZebuo9WkHpSngkAXYa
+valRecord=https://drive.google.com/uc?id=1Kn9Xbeurba9a5iBt0l8GsCrHwn5hFLE_
+
+echo "Setup pipeline in folder -> $path/TensorFlow"
+
 echo "Installing gdown"
 pip3 install gdown
 
@@ -9,21 +15,22 @@ mkdir workspace
 cd scripts
 mkdir preprocessing
 cd preprocessing
-cp /home/bragapo23/Pipeline-ObjectDetection/files/generate_tfrecord.py .
-cd /home/bragapo23/TensorFlow/workspace
+cp $path/Pipeline-ObjectDetection/files/generate_tfrecord.py .
+cd $path/TensorFlow/workspace
 mkdir training_demo
 cd training_demo
-cp /home/bragapo23/Pipeline-ObjectDetection/files/exporter_main_v2.py .
-cp /home/bragapo23/Pipeline-ObjectDetection/files/model_main_tf2.py .
+cp $path/Pipeline-ObjectDetection/files/exporter_main_v2.py .
+cp $path/Pipeline-ObjectDetection/files/model_main_tf2.py .
 mkdir annotations
 mkdir exported-models
 mkdir images
 mkdir models
 mkdir pre-trained-model
 cd annotations
-cp /home/bragapo23/Pipeline-ObjectDetection/files/label_map.pbtxt .
+echo "Copying custom label map"
+cp $path/Pipeline-ObjectDetection/files/label_map.pbtxt .
 echo "Downloading train.record"
-export PATH='/home/juanchovelezpro/.local/bin'
-gdown https://drive.google.com/uc?id=1Kl4dv4tg4ppFYfZebuo9WkHpSngkAXYa
+export PATH=$path/.local/bin
+gdown $trainRecord
 echo "Downloading test.record"
-gdown https://drive.google.com/uc?id=1Kn9Xbeurba9a5iBt0l8GsCrHwn5hFLE_
+gdown $valRecord
