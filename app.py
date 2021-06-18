eec43db9ae70b9600ebdf17769ac7fbed7989632
juanchovelezpro <velezolaya2012@hotmail.com>
2021-06-17 19:47:57 -0500

    Updating notebook and api

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
-from flask import Flask, request, Response, jsonify, send_from_directory, abort
-from absl import app, logging
-import os
+from flask import Flask, request, jsonify
 import numpy as np
-import json
 import tensorflow as tf
-from object_detection.utils import label_map_util
-from object_detection.utils import visualization_utils as viz_utils
 from PIL import Image
 import warnings
+
 warnings.filterwarnings('ignore')
 
 print("Loading Model...")
-model = tf.saved_model.load("./my_model/saved_model")
+model = tf.saved_model.load("./model/saved_model")
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<p>Object Detection API for OCR</p>"
+    return "<h1>Object Detection API for OCR</h1>"
 
 @app.route('/health', methods=['GET'])
 def health():
@@ -34,6 +30,6 @@ def detect():
     scores = detections['detection_scores'].numpy().tolist()
     classes = detections['detection_classes'].numpy().tolist()
     boxes = detections['detection_boxes'].numpy().tolist()
-    return jsonify({"successful":"test","scores":scores,"boxes":boxes,"classes":classes}), 200
-    
-app.run(host="0.0.0.0")
+    return jsonify({"scores":scores,"boxes":boxes,"classes":classes}), 200
+
+app.run(host='0.0.0.0')
